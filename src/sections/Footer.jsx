24b574ec523394dafe5f5047d3e60525194218af
tonykinuthia20@gmail.com <tontkinuthia20@gmail.com>
2025-04-18 19:59:02 +0300

    le final

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { socialImgs } from '../constants/index'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+        <div className='footer-container'>
+          <div className='socials'>
+          {socialImgs.map((img) => (
+            <a className='icon' target='_blank' href={img.url} key={img.name} rel='noreferrer'>
+                <img src={img.imgPath} alt={img.name}/>
+            </a>  
+          ))}
+          </div>
+
+          <div className='flex flex-col justify-center'>
+           <p className='text-center md:text-end'>
+            &copy; {new Date().getFullYear()} All rights reserved
+           </p>
+          </div>
+        </div>
+    </footer>
   )
 }
 
